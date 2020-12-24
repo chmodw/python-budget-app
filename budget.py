9e9087e4a3c99bf8b302e496453ecc03454f23c5
chamodya wimansha <chamodyawimansha@gmail.com>
2020-12-24 10:21:17 +0530

    percentage problem solved. all the tests are passing

--- budget.py
+++ budget.py
@@ -69,30 +69,39 @@ def create_spend_chart(categories):
     categoryNames = ""
     chart = ""
     maxCatLength = 0
+    TotalWithdraws = 0.0
+
+    # total withdraws of all the categories
+    for item in categories:
+        for ledgerItem in item.ledger:
+            if ledgerItem["amount"] < 0:
+                TotalWithdraws += abs(ledgerItem["amount"])
+            maxCatLength = len(item.category) if len(
+                item.category) > maxCatLength else maxCatLength
 
     for item in categories:
-        deposits = 0.0
         withdraws = 0.0
-
         for ledgerItem in item.ledger:
             if ledgerItem["amount"] < 0:
                 withdraws += abs(ledgerItem["amount"])
-            else:
-                deposits += ledgerItem["amount"]
 
         maxCatLength = len(item.category) if len(
             item.category) > maxCatLength else maxCatLength
 
         chartData.append({
             "category": item.category,
-            "percentage": int((deposits - (deposits - withdraws)) / deposits * 100)
+            "percentage": int((TotalWithdraws - (TotalWithdraws - withdraws)) / TotalWithdraws * 100)
         })
+
+    f = open("test.txt", "a")
+    f.write(str(chartData))
+    f.close()
 
     # creating the top part of the chart
     for i in reversed(range(11)):
         chart += "\n" + (" "*(3-len(str(i*10))))+str(i*10)+"| "
         for data in chartData:
-            if data["percentage"] > i*10:
+            if data["percentage"] >= i*10:
                 chart += "o  "
             else:
                 chart += "   "
@@ -109,30 +118,3 @@ def create_spend_chart(categories):
                 categoryNames += " "*3
 
     return "Percentage spent by category" + chart + dotLine + categoryNames
-
-# # print("Percentage spent by category\n100|          \n 90|          \n 80|          \n 70|    o     \n 60|    o     \n 50|    o     \n 40|    o     \n 30|    o     \n 20|    o  o  \n 10|    o  o  \n  0| o  o  o  \n    ----------\n     B  F  E  \n     u  o  n  \n     s  o  t  \n     i  d  e  \n     n     r  \n     e     t  \n     s     a  \n     s     i  \n           n  \n           m  \n           e  \n           n  \n           t  ")
-
-
-# print("done")
-food = Category("Food")
-entertainment = Category("Entertainment")
-business = Category("Business")
-
-food.deposit(900, "deposit")
-entertainment.deposit(900, "deposit")
-business.deposit(900, "deposit")
-food.withdraw(105.55)
-entertainment.withdraw(33.40)
-business.withdraw(10.99)
-
-print(create_spend_chart([business, food, entertainment]))
-
-
-# Problem is with percentage calculation
-
-# fix it and do the job assignment
-
-
-# f = open("s.txt", "w+")
-# f.write(create_spend_chart([business, food, entertainment]))
-# f.close()
